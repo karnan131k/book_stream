e3eb8505be7b6ba1746fb3cf0225420b7029e5c1
karnan131k <sooriyakumarkarnan@gmail.com>
2024-12-23 22:56:27 +0530

    project initial setup

---
+++ src/main/java/com/book/stream/BookStreamApplication.java
@@ -0,0 +1,13 @@
+package com.book.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/stream/BookStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
