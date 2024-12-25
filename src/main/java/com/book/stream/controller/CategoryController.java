736872e8b0b905d944482b9c7df1ce3a4d452795
karnan131k <sooriyakumarkarnan@gmail.com>
2024-12-25 12:28:57 +0530

    add category crud operations

--- src/main/java/com/book/stream/controller/CategoryController.java
+++ src/main/java/com/book/stream/controller/CategoryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Categories")
+@RequestMapping("/api/categories")
 public class CategoryController {
 
     private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
