6a1e461371c0a5b541ad86a3e18d0aedfecf5fbd
tharshika95 <tharshika.sathasivam.95@gmail.com>
2025-01-04 09:14:15 +0530

    add test greetings method

--- src/main/java/com/book/stream/controller/TestController.java
+++ src/main/java/com/book/stream/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.book.stream.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
-    // test purpose
+    public void greetings(){
+        System.out.println("welcome");
+    }
 }
